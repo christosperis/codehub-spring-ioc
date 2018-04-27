512b05848b343cc76a02a073d66a74a81e874ef6
christos <chrisperis@gmail.com>
2018-04-27 05:55:27 +0300

    ioc-demo Adds -Aware implementation for lifecycle demo

--- src/main/java/com/codehub/spring/impl/SoccerCoach.java
+++ src/main/java/com/codehub/spring/impl/SoccerCoach.java
@@ -1,8 +1,13 @@
 package com.codehub.spring.impl;
 
 import com.codehub.spring.Coach;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 
-public class SoccerCoach implements Coach {
+public class SoccerCoach implements Coach, BeanNameAware, BeanFactoryAware, ApplicationContextAware,
+        InitializingBean, DisposableBean{
 
     public static final String PERFORM_30_MIN_SOCCER_WORKOUT = "perform 30 min Soccer Workout";
 
@@ -17,5 +22,31 @@ public class SoccerCoach implements Coach {
 
     public void destroyMethod() {
         System.out.println("Inside Destroy Method");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("### Demo Bean has its properties set");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("### Demo Bean has been destroyed");
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("### Demo Bean name is " + name);
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("### Demo Bean Factory has been set ");
+
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("### Demo Bean display name is" + applicationContext.getDisplayName());
     }
 }
